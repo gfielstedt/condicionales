230e559dedb6043ecde6ef236f541171bfc73870
Ginessa <gfielstedt@gmail.com>
2024-01-15 00:51:06 -0300

    llamado de los 3 input

--- ej-2/js/index.js
+++ ej-2/js/index.js
@@ -1,6 +1,21 @@
 //elementos del DOM
 const inputUno = document.querySelector("#input-1");
 const inputDos = document.querySelector("#input-2");
+const inputTres = document.querySelector("#input-3");
+
 //variables
+
 //funciones
+
 //eventos
+
+btn.addEventListener("click", () => {
+  const valueUno = inputUno.value;
+  const valueDos = inputDos.value;
+  const valueTres = inputTres.value;
+
+  const total = Number(valueUno) + Number(valueDos) + Number(valueTres);
+
+  console.log(total);
+});
+3;
